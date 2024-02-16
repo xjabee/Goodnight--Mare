db533aedb4df322c60d67e9c016f82ad986dbe26
xJabee <jabee.sevilla@gmail.com>
2024-02-16 17:02:34 +0800

    Added Character feedback

--- Assets/Scripts/SlimeBehavior.cs
+++ Assets/Scripts/SlimeBehavior.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class SlimeBehaviour : MonoBehaviour
+public class SlimeBehavior : MonoBehaviour
 {
 
     public int HP = 2;
     int hitcount = 0;
+    public float jumpPower = 5;
+    public float jumpFrequency = 2f;
     SpriteRenderer sr;
     // Start is called before the first frame update
     public GameObject babySlime;
@@ -16,6 +18,13 @@ public class SlimeBehaviour : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("Bounce", 0f, jumpFrequency);
+    }
+
+    IEnumerator Bounce()
+    {
+        rb.AddForce(new Vector2(rb.velocity.x, jumpPower));
+        yield return new WaitForSeconds(1f);
     }
 
     
